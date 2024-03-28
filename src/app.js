bff57482bb0b23f225acf59505b6fa5cf841aca1
Bruno Alberstein <albersteinbruno@gmail.com>
2024-03-28 18:32:28 -0300

    desafío 4 listo para subir

--- src/app.js
+++ src/app.js
@@ -1,15 +1,52 @@
-import express from "express";
-import productRouter from "./router/products.routes.js";
-import cartRouter from "./router/carts.routes.js";
-
-const app = express();
+const express = require("express");
+const app = express(); 
 const PUERTO = 8080;
+const exphbs = require("express-handlebars");
+const socket = require("socket.io");
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
 
+//Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api/products", productRouter);
-app.use("/api/cart", cartRouter);
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("./src/public"));
 
-app.listen(PUERTO, () => {
-  console.log(`Escuchando en el puerto http://localhost:${PUERTO}`);
+
+//Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+//Rutas 
+app.use("/api", productsRouter);
+app.use("/api", cartsRouter);
+app.use("/", viewsRouter);
+
+
+
+const httpServer = app.listen(PUERTO, () => {
+    console.log(`Escuchando en el puerto: ${PUERTO}`);
+})
+
+const io = socket(httpServer);
+
+const ProductManager = require("./controllers/product-manager.js");
+const productManager = new ProductManager("./src/models/productos.json");
+
+
+io.on("connection", async (socket) => {
+    console.log("Un cliente conectado");
+
+    socket.emit("productos", await productManager.getProducts());
+
+    socket.on("eliminarProducto", async (id) => {
+        await productManager.deleteProduct(id);
+        socket.emit("productos", await productManager.getProducts());
+    })
+
+    socket.on("agregarProducto", async (producto) => {
+        await productManager.addProduct(producto);
+        socket.emit("productos", await productManager.getProducts());
+    })
 })
